27ee15670e9e29cb011ed012b99aef6269d85f4b
michael <mm.e.mihon@gmail.com>
2026-02-20 20:50:45 +0300

    feat: connect to redis in worker

--- services/worker/app.py
+++ services/worker/app.py
@@ -1,1 +1,20 @@
-print("worker test")
+import os
+
+import redis.asyncio as redis
+import phonenumbers
+from phonenumbers import carrier, geocoder
+
+
+# -----------------------------------------------------------------------------
+# Подключение к Redis
+# -----------------------------------------------------------------------------
+
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+
+redis_client = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    decode_responses=True,
+)
